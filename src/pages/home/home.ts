93be1528a269a20e61ecef0cc31c631c570a6d87
B.M.ASHIK MAHMUD <31992585+bmmahmud@users.noreply.github.com>
2018-06-11 22:23:09 +0600

    Verdion 1.0.1
    New add about and contact

---
+++ src/pages/home/home.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+ name1='';
+ name2='';
+get score(){
+const letters =(this.name1 + this.name2).toLowerCase();
+  let sum=0;
+  for(let i=0;i < letters.length; i++){
+    sum+= letters.charCodeAt(i);
+  }
+  return sum%101;
+}
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
